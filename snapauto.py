6ae076bdb28e8bf1b0b406fbb287f8ef83e2aa14
Anand Jayaraman <krvjanand@gmail.com>
2019-12-24 17:58:13 -0500

    Updated script to use click module and show more instance attributes

--- snapauto.py
+++ snapauto.py
@@ -1,8 +1,43 @@
 import boto3
+import click
+
+session = boto3.Session(profile_name='snapauto')
+ec2 = session.resource('ec2')
+
+@click.command("list")
+def list_instances():
+    "List EC2 instances"
+    cnt = 1
+    for i in ec2.instances.all():
+        if cnt == 1:
+            print('| '.join((
+                "Instance Id",
+                "Instance Type",
+                "AZ",
+                "State",
+                "Public DNS Name",
+                "Public IP",
+                "Public IP from NI")))
+            print('| '.join((
+                i.id,
+                i.instance_type,
+                i.placement['AvailabilityZone'],
+                i.state['Name'],
+                i.public_dns_name,
+                i.public_ip_address,
+                ''.join(list(map(lambda x : x['Association']['PublicIp'], i.network_interfaces_attribute))))))
+            cnt = cnt + 1
+        else:
+            cnt = cnt + 1
+            print('| '.join((
+                i.id,
+                i.instance_type,
+                i.placement['AvailabilityZone'],
+                i.state['Name'],
+                i.public_dns_name,
+                i.public_ip_address,
+                ''.join(list(map(lambda x : x['Association']['PublicIp'], i.network_interfaces_attribute))))))
+    return
 
 if __name__ == '__main__':
-    session = boto3.Session(profile_name='snapauto')
-    ec2 = session.resource('ec2')
-
-    for i in ec2.instances.all():
-        print(i)
+    list_instances()
