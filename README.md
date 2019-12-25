1ee2608e7a056ad46f362971b85fc0e2ee5e9459
Anand Jayaraman <krvjanand@gmail.com>
2019-12-25 00:32:50 -0500

    Improved Draft with added features to refactor code to filter instances; In addition added start and stop instance functions

--- README.md
+++ README.md
@@ -4,3 +4,14 @@ Python framework to list, start, stop EC2 instances, volumes and take snapshots 
 ## About
 
 This project uses boto3 package to manage EC2 instances running in Linux Academy's Cloud sandbox environment and list the instances
+
+## Configure
+
+snapauto uses config file created by AWS CLI e.g.
+
+'aws configure --profile snapauto'
+
+## running
+py snapauto.py <command> --project=Anand
+
+commands = list, start, stop instances
--- snapauto.py
+++ snapauto.py
@@ -4,18 +4,9 @@ import click
 session = boto3.Session(profile_name='snapauto')
 ec2 = session.resource('ec2')
 
-@click.command("list")
-@click.option('--project',default=None,
-    help="Only Instances for project (tag Project:<name>)")
-
-@click.option('--state', default=None,
-    help="Only Instances with state (tag state:<name>)")
-
-
-def list_instances(project, state):
-    "List EC2 instances"
+def filter_instances(project):
     instances = []
-    print('Project:',project)
+    # print('Project:',project)
     if project:
         filters=[{'Name':'tag:Project','Values': [project]}]
         instances = ec2.instances.filter(Filters=filters)
@@ -23,15 +14,37 @@ def list_instances(project, state):
     else:
         instances = ec2.instances.all()
 
-    if state:
-        stfilters=[{'Name':'instance-state-name','Values': [state]}]
-        instances = ec2.instances.filter(Filters=stfilters)
+    return instances
 
-    else:
-        instances = ec2.instances.all()
+
+@click.group()
+def instances():
+            "Commands for instances"
+
+@instances.command('list')
+@click.option('--project',default=None,
+        help="Only Instances for project (tag Project:<name>)")
+
+    # @click.option('--state', default=None,
+    #     help="Only Instances with state (tag state:<name>)")
+
+
+    # def list_instances(project, state):
+def list_instances(project):
+
+    "List EC2 instances"
+    instances=filter_instances(project)
+
+    # if state:
+    #     stfilters=[{'Name':'instance-state-name','Values': [state]}]
+    #     instances = ec2.instances.filter(Filters=stfilters)
+    #
+    # else:
+    #     instances = ec2.instances.all()
 
     cnt = 1
     for i in instances:
+        tags = {t['Key']: t['Value'] for t in i.tags or []}
         if cnt == 1:
             print('| '.join((
                 "Instance Id",
@@ -40,14 +53,16 @@ def list_instances(project, state):
                 "State",
                 "Public DNS Name",
                 "Public IP",
-                "Public IP from NI")))
+                "Public IP from NI",
+                "Tag")))
             print('| '.join((
                 i.id,
                 i.instance_type,
                 i.placement['AvailabilityZone'],
                 i.state['Name'],
                 i.public_dns_name,
-                str(i.public_ip_address))))
+                str(i.public_ip_address),
+                tags.get('Project','<No Project>'))))
 
                  # ''.join(list(map(lambda x: x['Association']['PublicIp'], i.network_interfaces_attribute))))))
             cnt = cnt + 1
@@ -60,9 +75,44 @@ def list_instances(project, state):
                 i.placement['AvailabilityZone'],
                 i.state['Name'],
                 i.public_dns_name,
-                str(i.public_ip_address))))
+                str(i.public_ip_address),
+                tags.get('Project','<No Project>'))))
                 # ''.join(list(map(lambda x: x['Association']['PublicIp'], i.network_interfaces_attribute))))))
     return
 
+@instances.command('stop')
+@click.option('--project',default=None,
+        help="Only Instances for project (tag Project:<name>)")
+
+def stop_instances(project):
+
+    "Stop EC2 instances"
+
+    instances=filter_instances(project)
+
+    for i in instances:
+        print("Stopping {0}...".format(i.id))
+        i.stop()
+
+
+    return
+
+@instances.command('start')
+@click.option('--project',default=None,
+        help="Only Instances for project (tag Project:<name>)")
+
+def start_instances(project):
+
+    "Start EC2 instances"
+
+    instances=filter_instances(project)
+
+    for i in instances:
+        print("Starting {0}...".format(i.id))
+        i.start()
+
+
+    return
+
 if __name__ == '__main__':
-    list_instances()
+    instances()
